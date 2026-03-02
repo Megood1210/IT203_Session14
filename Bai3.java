7df75754c898f4447caf8ab09cf38ed2fce1f861
Megood1210 <xuanvinhle06@gmail.com>
2026-03-02 09:56:31 +0700

    Upload bài tập 3

---
+++ Bai3.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        Set<String> drugComponents = new HashSet<>();
+        drugComponents.add("Aspirin");
+        drugComponents.add("Caffeine");
+        drugComponents.add("Paracetamol");
+
+        Set<String> allergyList = new HashSet<>();
+        allergyList.add("Penicillin");
+        allergyList.add("Aspirin");
+
+        System.out.println("Thuốc: " + drugComponents);
+        System.out.println("Dị ứng: " + allergyList);
+
+        Set<String> warning = new HashSet<>(drugComponents);
+        warning.retainAll(allergyList);
+
+        Set<String> safeComponents = new HashSet<>(drugComponents);
+        safeComponents.removeAll(allergyList);
+
+        System.out.println("Cảnh báo dị ứng: " + warning);
+        System.out.println("Thành phần an toàn: " + safeComponents);
+    }
+}
